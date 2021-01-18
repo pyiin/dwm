b3429508291b20378fb4725002baef5fb406bc72
pyiin <pyiin.ab@gmail.com>
2021-01-18 21:43:54 +0100

    colour change

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static int smartgaps          = 1;        /* 1 means no outer gap when there is 
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = {"Ubuntu Mono:size=15:antialias=true:autohint=true", "Ubuntu Mono Nerd Font:size=15:antialias=true:autohint=true", "JoyPixels:size=15:antialias=true:autohint=true"}; //,"monospace:size=15", "Font Awesome 5 Free:size=15:antialias=true:autohint=true",
-static const unsigned int baralpha = 0x50;
+static const unsigned int baralpha = 0x60;
 static const unsigned int borderalpha = OPAQUE;
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
